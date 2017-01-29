932c156413f356115b7a6fd379c55cce6a4a8ec0
Tatsuya Nakano <admin@howdylikes.jp>
2017-01-29 13:15:32 +0900

    Add test

---
+++ zoo/animals/animals_test.go
@@ -0,0 +1,30 @@
+package animals
+
+import "testing"
+
+func TestElephantFeed(t *testing.T) {
+	expect := "Grass"
+	actual := ElephantFeed()
+
+	if expect != actual {
+		t.Errorf("%s != %s", expect, actual)
+	}
+}
+
+func TestMonkeyFedd(t *testing.T) {
+	expect := "Banana"
+	actual := MonkeyFeed()
+
+	if expect != actual {
+		t.Errorf("%s != %s", expect, actual)
+	}
+}
+
+func TestRabbitFedd(t *testing.T) {
+	expect := "Carrot"
+	actual := RabbitFeed()
+
+	if expect != actual {
+		t.Errorf("%s != %s", expect, actual)
+	}
+}
